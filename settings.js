90868c19d2d0e2fedac6c91862f039d6c9fc6183
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-07-05 19:54:59 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'JINX-XMD',
+  author: '‎',
+  botName: "JINX-XMD",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '254112192119', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.6",
+};
+
+module.exports = settings;
