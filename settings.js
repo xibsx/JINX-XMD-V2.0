72aa5d42247c544f8e5b386817c5406c4f0ec44f
xibsx <xibstech@gmail.com>
2025-07-11 10:09:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'JINX-XMD',
   author: '‎',
-  botName: "JINX-XMD",
+  botName: "GIRAFFATITAN-XV",
   botOwner: 'Professor', // Your name
-  ownerNumber: '254112192119', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255754206718', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
